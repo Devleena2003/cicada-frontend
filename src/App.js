83072270490c17bb805b6b89ae2c65587a7d9c56
Devleena Das <95465835+Devleena2003@users.noreply.github.com>
2023-03-09 10:51:43 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,81 @@
+import React from "react";
+import "./App.css";
+import { Bar } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Tooltip,
+  Legend,
+  Chart,
+} from "chart.js";
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Tooltip, Legend);
+function App() {
+  const users = [
+    {
+      name: "xyz",
+
+      score: 70,
+    },
+    {
+      name: "abc",
+
+      score: 80,
+    },
+    {
+      name: "ijk",
+
+      score: 60,
+    },
+    {
+      name: "pqr",
+
+      score: 40,
+    },
+    {
+      name: "efg",
+
+      score: 20,
+    },
+  ];
+  const options = {
+    indexAxis: "y",
+    elements: {
+      bar: {
+        borderWidth: 2,
+      },
+      responsive: true,
+    },
+    plugins: {
+      legend: {
+        position: "right",
+      },
+    },
+  };
+  return (
+    <div className="App">
+      <div className="scoreboard-img"></div>
+      <div className="score-inner">
+        <h1 className="Scoreboard">Scoreboard</h1>
+        <Bar
+          style={{ margin: "5em", width: "50vw" }}
+          data={{
+            labels: users.map((user) => user.name),
+
+            datasets: [
+              {
+                label: "Score",
+                data: users.map((user) => user.score),
+                backgroundColor: "rgb(192, 255, 114)",
+              },
+            ],
+          }}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
